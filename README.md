b97c58ab37a2d7173afc4a04fb287df9dac0ea66
Tatr1 <ttttt_68@mail.ru>
2021-12-01 01:13:33 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AT-5.2
+[![Build status](https://ci.appveyor.com/api/projects/status/2jsw02ssgollsv2v?svg=true)](https://ci.appveyor.com/project/Tatr1/at-5-2)
+
---
+++ src/test/java/ru/netology/AuthTest.java
@@ -0,0 +1,85 @@
+package ru.netology;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.DataGenerator.Registration.getRegisteredUser;
+import static ru.netology.DataGenerator.Registration.getUser;
+import static ru.netology.DataGenerator.getRandomLogin;
+
+public class AuthTest {
+
+
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    @DisplayName("Should successfully login with active registered user")
+    void shouldSuccessfulLoginIfRegisteredActiveUser() {
+        var registeredUser = getRegisteredUser("active");
+        $("[data-test-id='login'] input").setValue(registeredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(registeredUser.getPassword());
+        $(withText("Продолжить")).click();
+        $(withText("Личный кабинет")).shouldHave(visible);
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with not registered user")
+    void shouldGetErrorIfNotRegisteredUser() {
+        var notRegisteredUser = getUser("active");
+        $("[data-test-id='login'] input").setValue(notRegisteredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(notRegisteredUser.getPassword());
+        $(withText("Продолжить")).click();
+        $(".notification__content").shouldBe(appear, Duration.ofSeconds(20)).shouldHave(text("Неверно указан логин или пароль"));
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with blocked registered user")
+    void shouldGetErrorIfBlockedUser() {
+        var blockedUser = getRegisteredUser("blocked");
+        $("[data-test-id='login'] input").setValue(blockedUser.getLogin());
+        $("[data-test-id='password'] input").setValue(blockedUser.getPassword());
+        $(withText("Продолжить")).click();
+        $(".notification__content").shouldBe(appear, Duration.ofSeconds(20)).shouldHave(text("Пользователь заблокирован"));
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with wrong login")
+    void shouldGetErrorIfWrongLogin() {
+        var registeredUser = getRegisteredUser("active");
+        var wrongLogin = getRandomLogin();
+        $("[data-test-id='login'] input").setValue(wrongLogin);
+        $("[data-test-id='password'] input").setValue(registeredUser.getPassword());
+        $(withText("Продолжить")).click();
+        $(".notification__content").shouldBe(appear, Duration.ofSeconds(20)).shouldHave(text("Неверно указан логин или пароль"));
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with wrong password")
+    void shouldGetErrorIfWrongPassword() {
+        var registeredUser = getRegisteredUser("active");
+        var wrongPassword = DataGenerator.getRandomPassword();
+        $("[data-test-id='login'] input").setValue(registeredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(wrongPassword);
+        $(withText("Продолжить")).click();
+        $(".notification__content").shouldBe(appear, Duration.ofSeconds(20)).shouldHave(text("Неверно указан логин или пароль"));
+    }
+
+    @Test
+    @DisplayName("Should get error message if input to null login and password")
+    void shouldGetErrorIfNullUser() {
+        $("[data-test-id='login'] input").setValue("");
+        $("[data-test-id='password'] input").setValue("");
+        $(withText("Продолжить")).click();
+        $(withText("Поле обязательно для заполнения")).shouldBe(appear);
+    }
+}
